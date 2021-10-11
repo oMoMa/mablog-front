4c9e9b18016039b37d24defca3db077c9b51c5dc
Mehrshad Shafaghi Omam <mehrshad.shafaghi@gmail.com>
2021-10-11 10:51:20 +0330

    change package name

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - wordpress-front',
-    title: 'wordpress-front',
+    titleTemplate: '%s - mablog-front',
+    title: 'mablog-front',
     htmlAttrs: {
       lang: 'en',
     },
