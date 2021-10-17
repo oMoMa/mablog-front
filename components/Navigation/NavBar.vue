f51d8f32c6bd9ec10ed15b0142e1e4a48e6bdef6
Mehrshad Shafaghi <mehrshad.shafaghi@gmail.com>
2021-10-18 02:38:26 +0330

    implement navigation bar

--- components/Navigation/NavBar.vue
+++ components/Navigation/NavBar.vue
@@ -47,44 +47,46 @@
         append-icon="mdi-magnify"
       ></v-text-field>
       <v-divider></v-divider>
-      <v-expansion-panels v-if="this.$auth.loggedIn">
-        <v-expansion-panel accordion>
-          <v-list>
-            <v-list-item class="px-2">
-              <v-list-item-avatar class="mx-2">
-                <img
-                  :src="this.$auth.user.profile_photo_url"
-                  alt="Profile Photo"
-                />
-              </v-list-item-avatar>
-            </v-list-item>
-            <v-list-item link>
-              <v-list-item-content>
-                <v-list-item-title class="text-h6">
-                  {{ this.$auth.user.name }}
-                </v-list-item-title>
-                <v-list-item-subtitle>{{
-                  this.$auth.user.email
-                }}</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-            <v-expansion-panel-content>
-              <v-list-item
-                link
-                @click="
-                  drawer = false
-                  userLogout()
-                "
-              >
-                <v-list-item-title>Logout</v-list-item-title>
-              </v-list-item>
-            </v-expansion-panel-content>
-          </v-list>
-        </v-expansion-panel>
-      </v-expansion-panels>
+
+      <v-list v-if="this.$auth.loggedIn">
+        <v-list-item class="px-2">
+          <v-list-item-avatar class="mx-2">
+            <img :src="this.$auth.user.profile_photo_url" alt="Profile Photo" />
+          </v-list-item-avatar>
+        </v-list-item>
+        <v-list-item link class="pa-0">
+          <v-expansion-panels>
+            <v-expansion-panel accordion>
+              <v-expansion-panel-header>
+                <v-list-item class="pa-0">
+                  <v-list-item-content>
+                    <v-list-item-title class="text-h6">
+                      {{ this.$auth.user.name }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle>{{
+                      this.$auth.user.email
+                    }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-list-item @click="closeDrawer" class="pa-0" link>
+                  <v-list-item-icon>
+                    <v-icon>mdi-logout-variant</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-title>Logout</v-list-item-title>
+                </v-list-item>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-list-item>
+      </v-list>
 
       <v-list v-else>
         <v-list-item link to="/login">
+          <v-list-item-icon>
+            <v-icon>mdi-login-variant</v-icon>
+          </v-list-item-icon>
           <v-list-item-title>Login</v-list-item-title>
         </v-list-item>
       </v-list>
@@ -103,10 +105,14 @@ export default {
   methods: {
     async userLogout() {
       try {
-        let response = await this.$auth.logout('local')
+        await this.$auth.logout('local')
       } catch (err) {
         console.error(err)
       }
+    },
+    closeDrawer() {
+      this.drawer = false
+      this.userLogout()
     },
   },
 }
